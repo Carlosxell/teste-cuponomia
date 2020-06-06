e30622f9c44a60e712e1910087a24cd9afb6c577
Carlos Augusto <carlos.xell.3@gmail.com>
2020-06-06 10:42:23 -0300

    primeiro commit de projeto [HTML, Js, Css]

---
+++ src/components/Table.vue
@@ -0,0 +1,36 @@
+<template>
+  <table class="c-table">
+    <thead class="c-table_head">
+      <tr class="c-table_head_tr">
+        <th>Id</th>
+        <th>Nome</th>
+        <th>Usuário</th>
+        <th>Website</th>
+      </tr>
+    </thead>
+
+    <tbody class="c-table_body">
+      <tr class="c-table_body_tr"
+          :key="ind"
+          v-for="(item, ind) in propList">
+        <td>{{ JSON.stringify(item) }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+  export default {
+    name: 'Table',
+    props: {
+      propList: {
+        type: Array,
+        default: () => { return []; }
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import '../assets/Css/_utilities/_extends';
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="l-page--home">
+    <div class="l-container">
+
+      <button @click="searchUsers"
+              class="c-btn"
+              type="button">Mostrar usuários</button>
+
+      <Table :prop-list="usersList"
+             v-if="!usersList.length" />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+  import Table from '../components/Table';
+  import { getUsers } from '../services/users.service';
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
-};
+  export default {
+    name: 'Home',
+    data: () => {
+      return {
+        usersList: []
+      };
+    },
+    components: { Table },
+    methods: {
+      searchUsers() {
+        getUsers().then(res => this.usersList = res);
+      }
+    }
+  };
 </script>
+
+<style lang="scss">
+  @import '../assets/Css/_style';
+</style>
